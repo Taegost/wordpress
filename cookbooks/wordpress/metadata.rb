0dbe8500a83a2f2af248811f6477241baa6e93e2
Mike Wheway <taegost@gmail.com>
2019-05-16 16:50:52 -0400

    Updated metadata with URLs

--- cookbooks/wordpress/metadata.rb
+++ cookbooks/wordpress/metadata.rb
@@ -11,13 +11,13 @@ chef_version '>= 14.0'
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
 # uploaded to a Supermarket.
 #
-# issues_url 'https://github.com/<insert_org_here>/wordpress/issues'
+issues_url 'https://github.com/taegost/wordpress/issues'
 
 # The `source_url` points to the development repository for this cookbook.  A
 # `View Source` link will be displayed on this cookbook's page when uploaded to
 # a Supermarket.
 #
-# source_url 'https://github.com/<insert_org_here>/wordpress'
+source_url 'https://github.com/taegost/wordpress'
 
 depends 'yum-epel', '= 3.3.0'
 depends 'yum-remi-chef', '= 3.0.0'
